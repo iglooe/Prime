b60458e0a4726b03dd2266ee52153356bf8be490
Derek Day <day.derek7@protonmail.com>
2023-03-21 08:32:54 -0500

    bug fixes, added more features

--- src/assets/app.tsx
+++ src/assets/app.tsx
@@ -4,6 +4,10 @@ import Header from "./components/header";
 function App() {
   const [number, setNumber] = React.useState("");
   const [isPrime, setIsPrime] = React.useState(false);
+  const [headingText, setHeadingText] = React.useState(
+    "Try it out! Enter a number"
+  );
+  const [shakeScreen, setShakeScreen] = useState<boolean>(false);
 
   function isNumberPrime(num: number) {
     if (num == 2 || num == 3) return true;
@@ -13,21 +17,38 @@ function App() {
     return true;
   }
 
-  function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
-    const num = Number(e.target.value);
-    setNumber(num.toString());
+  function handleKeyPress(e: React.KeyboardEvent<HTMLInputElement>) {
+    const enteredChar = e.key;
+    const isNumber = /^[0-9]$/;
+    if (
+      !isNumber.test(enteredChar) &&
+      enteredChar !== "Backspace" &&
+      enteredChar !== "ArrowLeft" &&
+      enteredChar !== "ArrowRight" &&
+      enteredChar !== "Delete"
+    ) {
+      e.preventDefault();
+    }
+
+    const isLetter = /^[a-zA-Z]$/;
+    if (isLetter.test(enteredChar)) {
+      setShakeScreen(true);
+    } else {
+      setShakeScreen(false);
+    }
   }
 
   function handleButtonClick() {
     const num = Number(number);
     if (num <= 1 || isNaN(num)) {
       setIsPrime(false);
-    } else {
+    } else if (num > 1) {
       setIsPrime(isNumberPrime(num));
     }
+    console.log("clicked");
 
-    const heading = document.querySelector(".heroTitle") as HTMLElement;
-    heading.textContent = isPrime ? "Prime" : "Not Prime";
+    const headingText = isPrime ? "Prime" : "Not Prime";
+    setHeadingText(headingText);
   }
 
   return (
@@ -35,24 +56,29 @@ function App() {
       <Header />
       <div className="container">
         <div className="content">
-          <h3 className="heroTitle">Try it out! Enter a number.</h3>
-          <input
-            type="text"
-            name="numInput"
-            placeholder=""
-            value={number}
-            onChange={handleInputChange}
-          />
-          <button
-            type="submit"
-            className="btn btn-large btn-primary btn-block"
-            value="input"
-            onClick={handleButtonClick}
-          >
-            Check
-          </button>
+          <h3 className="heroTitle">{headingText}.</h3>
+          <div className={`App ${shakeScreen ? "shake" : ""}`}>
+            <input
+              type="text"
+              name="numInput"
+              placeholder=""
+              value={number}
+              onKeyDown={handleKeyPress}
+              onChange={(e) => setNumber(e.target.value)}
+            />
+            <button
+              type="submit"
+              className="btn btn-large btn-primary btn-block"
+              value="input"
+              onClick={handleButtonClick}
+            >
+              Check
+            </button>
+          </div>
+
           <div className="explanation">
-            <h4>About the algorithm used...</h4>
+            <h4 className="h4">About the algorithm used...</h4>
+            <img src="../public/images/primality.png" alt="algorithm img" />
             <p className="e-text">
               Let's consider the number 100, which is evenly divisible by these
               numbers
@@ -75,6 +101,46 @@ function App() {
             <p className="e-text">
               Notice that products past <em>10 x 10</em> simply repeat numbers
               which appears in earlier products.
+            </p>
+            <p className="e-text">
+              For example 20 x 5 and 5 x 20 consist of the same numbers in
+              opposite order.
+            </p>
+            <p className="e-text">
+              All unique factors are less than or equal to &#x221A;n (the square
+              root of n). We do not need to search past that.
+            </p>
+            <p className="e-text">
+              All even numbers greater than 2 can also be eliminated.
+            </p>
+            <p className="e-text">
+              We improve this method further observing all primes greater than 3
+              are of the form:
+            </p>
+            <p className="code-block">
+              6k<span>&#177;1</span> where <em>k</em> is any integer greater
+              than 0.
+            </p>
+            <p className="e-text">
+              This is because all integers can be expressed as:
+            </p>
+            <p className="code-block">
+              (6k<span>&#177;i)</span> where <em>i</em> = -1, 0, 1, 2, 3, or 4.
+            </p>
+            <p className="e-text">
+              Note that 2 divides <em>4k + 0</em> and <em>4k + 2</em>. and 3
+              divides <em>4k + 3</em>
+            </p>
+            <p className="e-text">
+              So, a more efficient method is to test whether <em>n</em> is
+              divisible by 2 or 3
+            </p>
+            <p className="e-text">
+              Then to check through all the numbers of form{" "}
+              <em>6k &#177;1 &#8804; &#x221A;n</em>
+            </p>
+            <p className="e-text">
+              This is 3 times faster than testing all numbers up to &#x221A;n
             </p>
           </div>
         </div>
